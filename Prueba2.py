3e71c86df9c1d19362f25df5bdd145c8bee9db0f
davidmato <150828813+davidmato@users.noreply.github.com>
2023-11-14 17:31:19 +0100

    creacion variable operacion Prueba2.py

--- Prueba2.py
+++ Prueba2.py
@@ -1,6 +1,8 @@
-suma = 2 + 2
+operacion = int(input("escribe un numero"))
+suma = 2 + operacion
 print(f"Resultado es: {suma}")
 if suma > 3:
   print('pedro sanchez ganó esta batalla')
 else:
   print('españa se está despertando')
+  
