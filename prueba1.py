eed99fd76c2f3ab622fbb22855ed054bf910b1d2
raulcruzadodelgado <145713491+raulcruzadodelgado@users.noreply.github.com>
2023-11-14 17:08:01 +0100

    Update prueba1.py
    porro de maria verde y azul

--- prueba1.py
+++ prueba1.py
@@ -3,14 +3,14 @@
 import random
 
 def adivinar_numero():
-    num = random.randint(1, 10)
+    num = random.randint(1, 20)
     num_usuario = 0
     vidas = 0
     while num_usuario != num:
-        num_usuario = str(input("Escribe un número (del 1 al 10) -->"))
+        num_usuario = str(input("Escribe un número (del 1 al 20) -->"))
 
         while not num_usuario.isdigit():
-            num_usuario = str(input("No es un número, escribe un número (del 1 al 10) -->"))
+            num_usuario = str(input("No es un número, escribe un número (del 1 al 20) -->"))
 
         num_usuario = int(num_usuario)
 
